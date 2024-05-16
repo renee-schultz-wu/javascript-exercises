293a900b4548627aea4dcc6741c50106c5a35143
renee <50965960+wurenee@users.noreply.github.com>
2024-05-15 21:06:59 -0700

    Modified leapYears.js

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (!Number.isInteger(year)){return "ERROR"}
+    if (year % 4 != 0) {return false}
+    else if (year % 100 == 0 && year % 400 != 0) {return false}
+    else {return true}
 };
 
 // Do not edit below this line
