2fcd41a30345a5524fa3d2512646c02a16a4ca74
renee <50965960+wurenee@users.noreply.github.com>
2024-05-15 20:52:25 -0700

    Modified reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const n = string.length
+    if (n == 0){
+        return ""
+    }
+    let res = ""
+    for (let i = 0; i < n; i++){
+        res += string.slice(n - i - 1, n - i)
+    }
+    return res
 };
 
 // Do not edit below this line
